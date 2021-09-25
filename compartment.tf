4a100ddc5028abb1bae4b0f551cc7d6741cb5597
Felipe Eduardo Ribeiro <felipe@redix.com.br>
2021-09-25 19:25:58 -0300

    Changes to deploy

--- compartment.tf
+++ compartment.tf
@@ -1,5 +1,5 @@
 resource "oci_identity_compartment" "compart_main" {
-    compartment_id = var.compartment_root_id
-    description = var.compartment
-    name = var.compartment
+  compartment_id = var.compartment_root_id
+  description    = var.compartment
+  name           = var.compartment
 }
