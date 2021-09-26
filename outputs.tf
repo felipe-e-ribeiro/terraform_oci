7d1a7b62372bc45f02c5673af87319c9a4eb1eaf
Felipe Eduardo Ribeiro <felipe@redix.com.br>
2021-09-26 00:14:34 -0300

    Change

--- outputs.tf
+++ outputs.tf
@@ -6,24 +6,18 @@ output "private-security-list-OCID" {
   value = oci_core_security_list.private-security-list.id
 }
 
-# Outputs for public security list
-
-#output "instance_private_and_public_ips" {
-#  value = oci_core_instance.CreateInstance["server1"].public_ip
-#    for instance_variables in oci_core_instance.CreateInstance.instance_variables : instance_variables.public_ip
-#   instance => instance.public_ip
-#value = {
-#for u in oci_core_instance.CreateInstance : u.public_ip    
-#}
-#}
-
-#output "instance_private_and_public_ips2" {
-#  value = oci_core_instance.CreateInstance["server2"].public_ip
-#}
-
 data "oci_core_instances" "test_instances" {
   compartment_id = oci_identity_compartment.compart_main.id
 }
+
+output "public_hostname" {
+  value = {
+    for k, v in oci_core_instance.CreateInstance : k => v.display_name
+  }
+
+  depends_on = [oci_core_instance.CreateInstance[0]]
+}
+
 
 output "public_ips" {
   value = {
@@ -32,6 +26,7 @@ output "public_ips" {
 
   depends_on = [oci_core_instance.CreateInstance[0]]
 }
+
 
 output "private-subnet-name" {
   value = oci_core_subnet.vcn-private-subnet.display_name
