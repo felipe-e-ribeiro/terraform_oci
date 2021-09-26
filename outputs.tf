673474c9409d5452747dd058365b020bfc0d4e0d
Felipe Eduardo Ribeiro <felipe@redix.com.br>
2021-09-26 00:16:01 -0300

    Change

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,4 @@
 
-output "private-security-list-name" {
-  value = oci_core_security_list.private-security-list.display_name
-}
-output "private-security-list-OCID" {
-  value = oci_core_security_list.private-security-list.id
-}
-
 data "oci_core_instances" "test_instances" {
   compartment_id = oci_identity_compartment.compart_main.id
 }
@@ -18,7 +11,6 @@ output "public_hostname" {
   depends_on = [oci_core_instance.CreateInstance[0]]
 }
 
-
 output "public_ips" {
   value = {
     for k, v in oci_core_instance.CreateInstance : k => v.public_ip
@@ -26,12 +18,3 @@ output "public_ips" {
 
   depends_on = [oci_core_instance.CreateInstance[0]]
 }
-
-
-output "private-subnet-name" {
-  value = oci_core_subnet.vcn-private-subnet.display_name
-}
-output "private-subnet-OCID" {
-  value = oci_core_subnet.vcn-private-subnet.id
-}
-#
