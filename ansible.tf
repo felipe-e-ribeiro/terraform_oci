6b2b31bccb63bcd796afca6edccbbf3ea819da2c
Felipe Eduardo Ribeiro <felipe@redix.com.br>
2021-10-05 19:23:44 -0300

    Melhorias Ansible

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "AnsibleProvisioning" {
   provisioner "local-exec" {
-  command = "sleep 120; export ANSIBLE_HOST_KEY_CHECKING=False ; ansible-playbook -i \"./ansible/inventory\" --private-key \"./credentials/private_key\" ./ansible/docker.yml"
- }
+    command = "sleep 120; export ANSIBLE_HOST_KEY_CHECKING=False ; ansible-playbook -i \"./ansible/inventory\" --private-key \"./credentials/private_key\" ./ansible/docker.yml"
+  }
 }
 
