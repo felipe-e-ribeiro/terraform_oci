af86776c586edbff421682e6751b44e27505ea91
Felipe Eduardo Ribeiro <felipe@redix.com.br>
2021-10-23 00:55:33 -0200

    Melhorando ansible

--- ansible.tf
+++ ansible.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "AnsibleProvisioning" {
+ triggers = {
+      build_number = "${timestamp()}"
+  }
   provisioner "local-exec" {
     command = "sleep 180; export ANSIBLE_HOST_KEY_CHECKING=False ; ansible-playbook -i \"./ansible/inventory\" --private-key \"./credentials/private_key\" ./ansible/docker.yml"
   }
